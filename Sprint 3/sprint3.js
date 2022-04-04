51ec6485d5f32e36588b132a13c82fde5782efbe
YodoxD <sozial.oeschger@gmail.com>
2022-04-05 01:46:34 +0200

    Sprite 3

--- Sprint 3/sprint3.js
+++ Sprint 3/sprint3.js
@@ -1,1 +1,7 @@
-gsap.from('.txtBox',{ duration: 3, y: '-100%', ease: "bounce" })
+gsap.from('.txtEgo',{ duration: 6, y: '-100%', ease: "bounce" })
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.to('.txtEgo', {
+    duration: 10,
+    scrollTrigger: '.txtEgo'
+})
